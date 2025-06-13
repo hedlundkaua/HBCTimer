f16938ed21ca85b62434d73e40b1a7e1f0b54257
hedlundkaua <hedlundkaua@gmail.com>
2025-06-13 14:43:09 -0300

    Iniciando o projeto

---
+++ src/main/java/com/HBCTimerAPI/HbcTimerApplication.java
@@ -0,0 +1,13 @@
+package com.HBCTimerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbcTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbcTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HBCTimerAPI/HbcTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HBCTimerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbcTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
