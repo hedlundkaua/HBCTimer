eed234127097878c2647bf52a056be69f365236d
hedlundkaua <hedlundkaua@gmail.com>
2025-06-14 19:15:53 -0300

    Config implementation

--- src/main/java/com/HBCTimerAPI/controller/UserController.java
+++ src/main/java/com/HBCTimerAPI/controller/UserController.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.HBCTimerAPI.model.entities.User;
 import com.HBCTimerAPI.services.UserService;
 
-@RequestMapping("/users")
 @RestController
+
+@RequestMapping(value = "/users")
 public class UserController {
 
+	
+	@Autowired
 	private UserService service;
 	
 	@GetMapping
@@ -26,8 +31,14 @@ public class UserController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(user);
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 		
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		User user = service.insert(obj);
+		return ResponseEntity.ok().body(user);
+	}
+	
 }
--- src/main/java/com/HBCTimerAPI/model/entities/User.java
+++ src/main/java/com/HBCTimerAPI/model/entities/User.java
@@ -1,30 +1,55 @@
 package com.HBCTimerAPI.model.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.Getter;
+import jakarta.persistence.Table;
+
 
 @Entity
-@Getter
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
+
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
+	
 	private String name;
 	private String email;
 	private String password;
 	
-	@OneToMany
-	private Set<DailyGoal> only = new HashSet<>(); 
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
 	
 }
