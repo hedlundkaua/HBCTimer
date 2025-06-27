7c9e9c167f15956b683040900aec7b0e78238775
hedlundkaua <hedlundkaua@gmail.com>
2025-06-26 22:11:29 -0300

    refactored DTO packages #36

---
+++ src/main/java/com/HBCTimerAPI/dto/user/UserProfileDTO.java
@@ -0,0 +1,27 @@
+package com.HBCTimerAPI.dto.user;
+
+import com.HBCTimerAPI.model.entities.User;
+
+public class UserProfileDTO {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public User criaObjeto() {
+		return new User(id, name, email, password);
+	}	
+}
