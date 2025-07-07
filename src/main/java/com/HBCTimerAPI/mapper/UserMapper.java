b8c78ca8faca19020647ad68203ed122bc143c69
hedlundkaua <hedlundkaua@gmail.com>
2025-07-07 16:42:42 -0300

    Created UserMapper #46

---
+++ src/main/java/com/HBCTimerAPI/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package com.HBCTimerAPI.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.HBCTimerAPI.domain.entities.User;
+import com.HBCTimerAPI.dto.user.UserRequestDTO;
+import com.HBCTimerAPI.dto.user.UserResponseDTO;
+
+public class UserMapper {
+
+	public static User toEntity(UserRequestDTO dto) {
+		return new User(null, dto.getName(), dto.getEmail(), dto.getPassword());
+	}
+	
+	public static UserResponseDTO toDTO(User user) {
+		UserResponseDTO dto = new UserResponseDTO();
+		dto.setId(user.getId());
+		dto.setName(user.getName());
+		dto.setEmail(user.getEmail());
+		return dto;
+	}
+	
+	public static List<UserResponseDTO> toDTOList(List<User> user){
+		return user.stream()
+		.map(UserMapper::toDTO)
+		.collect(Collectors.toList());
+	}
+	
+	
+	
+}
